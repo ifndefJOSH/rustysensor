463013a8f2f32f4585b8f5f0eff5e838baf13aae
Joshua Jeppson <joshua.jeppson17@gmail.com>
2023-11-20 13:25:34 -0700

    Added some electro_optical stuff

--- rustysensor/src/electro_optical.rs
+++ rustysensor/src/electro_optical.rs
@@ -1,2 +1,183 @@
 
 // ===================== Electro Optical Systems =====================
+
+mod el_opt {
+	mod tables {
+		#[derive(Clone, Debug)]
+		struct Range {
+			index : u8
+			, lbound : f64
+			, ubound : f64
+		}
+		let modis = [Range { index : 1, lbound : 6.2e-07, ubound : 6.7e-07 },
+					Range { index : 2, lbound : 8.41e-07, ubound : 8.76e-07 },
+					Range { index : 3, lbound : 4.59e-07, ubound : 4.79e-07 },
+					Range { index : 4, lbound : 5.45e-07, ubound : 5.65e-07 },
+					Range { index : 5, lbound : 1.23e-06, ubound : 1.25e-06 },
+					Range { index : 6, lbound : 1.628e-06, ubound : 1.652e-06 },
+					Range { index : 7, lbound : 2.105e-06, ubound : 2.155e-06 },
+					Range { index : 8, lbound : 4.05e-07, ubound : 4.2e-07 },
+					Range { index : 9, lbound : 4.38e-07, ubound : 4.48e-07 },
+					Range { index : 10, lbound : 4.84e-07, ubound : 4.93e-07 },
+					Range { index : 11, lbound : 5.26e-07, ubound : 5.36e-07 },
+					Range { index : 12, lbound : 5.46e-07, ubound : 5.56e-07 },
+					Range { index : 13, lbound : 6.62e-07, ubound : 6.72e-07 },
+					Range { index : 14, lbound : 6.73e-07, ubound : 6.83e-07 },
+					Range { index : 15, lbound : 7.43e-07, ubound : 7.53e-07 },
+					Range { index : 16, lbound : 8.62e-07, ubound : 8.77e-07 },
+					Range { index : 17, lbound : 8.9e-07, ubound : 9.2e-07 },
+					Range { index : 18, lbound : 9.31e-07, ubound : 9.41e-07 },
+					Range { index : 19, lbound : 9.15e-07, ubound : 9.65e-07 }];
+
+		let ocm_2 = [Range { index : 1, lbound : 4.04e-07, ubound : 4.24e-07 },
+					Range { index : 2, lbound : 4.31e-07, ubound : 4.51e-07 },
+					Range { index : 3, lbound : 4.76e-07, ubound : 4.96e-07 },
+					Range { index : 4, lbound : 5e-07, ubound : 5.2e-07 },
+					Range { index : 5, lbound : 5.46e-07, ubound : 5.66e-07 },
+					Range { index : 6, lbound : 6.1e-07, ubound : 6.3e-07 },
+					Range { index : 7, lbound : 7.25e-07, ubound : 7.55e-07 },
+					Range { index : 8, lbound : 8.45e-07, ubound : 8.85e-07 }];
+	}
+	#[requires(wavelength > 0, "Wavelength must be greater than 0")]
+	#[requires(d > 0 && d < 1, "Distance must be nonzero, but not too large")]
+	#[ensures(ret > 0 && ret < 6.29)] // radians
+	fn diffraction_angle(n : u32, wavelength : f64, d : f64) -> f64 {
+		return ((n as f64) * wavelength / d).asin();
+	}
+
+	#[requires(lambda >= 0.52e-6 && lambda <= 2.43e-6, "Wavelength must be in VNIR region!")]
+	#[ensures(ret > 0 && ret < 10)]
+	fn aster(lamda : f64) -> u8 {
+		if lambda =< 0.6e-6 {
+			return 1;
+		}
+		else if lambda >= 0.63e-6 && lambda <= 0.69e-6 {
+			return 2;
+		}
+		// Does not specify 3n vs 3b
+		else if lambda >= 0.76e-6 && lambda <= 0.86e-6 {
+			return 3;
+		}
+		else if lambda >= 1.6e-6 && lambda <= 1.7e-6 {
+			return 4;
+		}
+		else if lambda >= 2.145e-6 && lambda <= 2.185e-6 {
+			return 5;
+		}
+		else if lambda >= 2.185e-6 && lambda <= 2.225e-6 {
+			return 6;
+		}
+		else if lambda >= 2.235e-6 && lambda <= 2.285e-6 {
+			return 7;
+		}
+		else if lambda >= 2.295e-6 && lambda <= 2.365e-6 {
+			return 8;
+		}
+		else if lambda >= 2.365e-6 && lambda <= 2.430e-6 {
+			return 9;
+		}
+		else {
+			assert!(false, "Invalid aser wavelength");
+			return 1; // make rustc happy
+		}
+	}
+
+	#[requires(lambda >= 4.05e-7 && lambda <= 2.155e-6, "Wavelength must be in accurate MODIS region!")]
+	#[ensures(ret > 0 && ret < 19)]
+	fn modis(lambda : f64) -> u8 {
+		if lambda >= 6.2e-07 && lambda <= 6.7e-07 {
+			return 1;
+		}
+		else if lambda >= 8.41e-07 && lambda <= 8.76e-07 {
+			return 2;
+		}
+		else if lambda >= 4.59e-07 && lambda <= 4.79e-07 {
+			return 3;
+		}
+		else if lambda >= 5.45e-07 && lambda <= 5.65e-07 {
+			return 4;
+		}
+		else if lambda >= 1.23e-06 && lambda <= 1.25e-06 {
+			return 5;
+		}
+		else if lambda >= 1.628e-06 && lambda <= 1.652e-06 {
+			return 6;
+		}
+		else if lambda >= 2.105e-06 && lambda <= 2.155e-06 {
+			return 7;
+		}
+		else if lambda >= 4.05e-07 && lambda <= 4.2e-07 {
+			return 8;
+		}
+		else if lambda >= 4.38e-07 && lambda <= 4.48e-07 {
+			return 9;
+		}
+		else if lambda >= 4.84e-07 && lambda <= 4.93e-07 {
+			return 10;
+		}
+		else if lambda >= 5.26e-07 && lambda <= 5.36e-07 {
+			return 11;
+		}
+		else if lambda >= 5.46e-07 && lambda <= 5.56e-07 {
+			return 12;
+		}
+		else if lambda >= 6.62e-07 && lambda <= 6.72e-07 {
+			return 13;
+		}
+		else if lambda >= 6.73e-07 && lambda <= 6.83e-07 {
+			return 14;
+		}
+		else if lambda >= 7.43e-07 && lambda <= 7.53e-07 {
+			return 15;
+		}
+		else if lambda >= 8.62e-07 && lambda <= 8.77e-07 {
+			return 16;
+		}
+		else if lambda >= 8.9e-07 && lambda <= 9.2e-07 {
+			return 17;
+		}
+		else if lambda >= 9.31e-07 && lambda <= 9.41e-07 {
+			return 18;
+		}
+		else if lambda >= 9.15e-07 && lambda <= 9.65e-07 {
+			return 19;
+		}
+		else {
+			assert!(false, "Invalid modis wavelength");
+			return 1; // make rustc happy
+		}
+	}
+
+	#[requires(lambda >= 4.04e-7 && lambda <= 8.85-7, "Wavelength must be in accurate OCM-2 region!")]
+	#[ensures(ret > 0 && ret < 8)]
+	fn ocm_2(lambda : f64 -> u8 {
+		if lambda >= 4.04e-07 && lambda <= 4.24e-07 {
+			return 1;
+		}
+		else if lambda >= 4.31e-07 && lambda <= 4.51e-07 {
+			return 2;
+		}
+		else if lambda >= 4.76e-07 && lambda <= 4.96e-07 {
+			return 3;
+		}
+		else if lambda >= 5e-07 && lambda <= 5.2e-07 {
+			return 4;
+		}
+		else if lambda >= 5.46e-07 && lambda <= 5.66e-07 {
+			return 5;
+		}
+		else if lambda >= 6.1e-07 && lambda <= 6.3e-07 {
+			return 6;
+		}
+		else if lambda >= 7.25e-07 && lambda <= 7.55e-07 {
+			return 7;
+		}
+		else if lambda >= 8.45e-07 && lambda <= 8.85e-07 {
+			return 8;
+		}
+		else {
+			assert!(false, "Invalid modis wavelength");
+			return 1; // make rustc happy
+		}
+	}
+}
--- rustysensor/src/em.rs
+++ rustysensor/src/em.rs
@@ -1,111 +1,149 @@
 
-// ===================== Constants =====================
-// These values taken from wikipedia
-const C : f64            = 299792458;
-// Free space constants
-const MU_0 : f64         = 1.25663706212e-6;
-const EPSILON_0_SI : f64 = 8.8541878128e-12;
-const EPSILON_0_EV : f64 = 55.26349406;
-const Z0 : f64           = sqrt(MU_0 / EPSILON_0_SI);
-const Z0_EV : f64        = sqrt(MU_0 / EPSILON_0_EV);
-// Other math constants
-const PI : f64           = 3.141592653589793;
-// Boltzmann constant
-const K : f64            = 1.380649e-23;
-const SIGMA : f64        = 5.670367e-8;
-// Wave numbers
-const K_E_SI : f64       = 1 / (4 * PI * EPSILON_0_SI);
-const K_E_EV : f64       = 1 / (4 * PI * EPSILON_0_EV);
-// Planck Constant
-const H : f64            = 6.62607015e-34;
-const H_EV : f64         = 4.135667696e-15;
-// Earth blackbody irradiance
-const EARTH_IRRAD : f64  = 1.37e3;
-// Mean exoatmospheric irradiance
-const EXOATMO_RAD : f64  = 2.02e7;
-
 // ===================== Basic EM in free space =====================
 
-fn angular_frequency(f : f64) -> f64 {
-    return f * 2 * PI;
+mod em {
+	use super::*;
+	mod consts {
+		use super::*;
+		// ===================== Constants =====================
+		// These values taken from wikipedia
+		const C : u32            = 299792458;
+		// Free space constants
+		const MU_0 : f64         = 1.25663706212e-6;
+		const EPSILON_0_SI : f64 = 8.8541878128e-12;
+		const EPSILON_0_EV : f64 = 55.26349406;
+		const Z0 : f64           = sqrt(MU_0 / EPSILON_0_SI);
+		const Z0_EV : f64        = sqrt(MU_0 / EPSILON_0_EV);
+		// Other math constants
+		const PI : f64           = 3.141592653589793;
+		// Boltzmann constant
+		const K : f64            = 1.380649e-23;
+		const SIGMA : f64        = 5.670367e-8;
+		// Wave numbers
+		const K_E_SI : f64       = 1 / (4 * PI * EPSILON_0_SI);
+		const K_E_EV : f64       = 1 / (4 * PI * EPSILON_0_EV);
+		// Planck Constant
+		const H : f64            = 6.62607015e-34;
+		const H_EV : f64         = 4.135667696e-15;
+		// Earth blackbody irradiance
+		const EARTH_IRRAD : f64  = 1.37e3;
+		// Mean exoatmospheric irradiance
+		const EXOATMO_RAD : f64  = 2.02e7;
+
+	}
+	use consts::*;
+	#[requires(f > 0, "Frequency must be greater than zero Hz!")]
+	#[ensures(ret > 0)]
+	#[debug_ensures(ret == f * 2 * PI)]
+	fn angular_frequency(f : f64) -> f64 {
+		return f * 2 * PI;
+	}
+
+	#[requires(f > 0, "Frequency must be greater than zero Hz!")]
+	#[ensures(ret > 0)]
+	#[debug_ensures(ret == C / f)]
+	fn em_wavelength(f : f64) -> f64 {
+		return C / f;
+	}
+
+	#[requires(lambda > 0, "Wavelength must be greater than zero!")]
+	#[ensures(ret > 0)]
+	#[debug_ensures(ret == C / lambda)]
+	fn em_frequency(lambda : f64) -> f64 {
+		return C / lambda;
+	}
+
+	#[requires(lambda > 0, "Wavelength must be greater than zero!")]
+	fn wave_num(lambda : f64) -> u64 {
+		return (2 * PI / lambda) as u64;
+	}
+
+	#[requires(f > 0, "Frequency must be greater than zero!")]
+	#[ensures(ret > 0)]
+	fn photon_energy(f : f64) -> f64 {
+		return H * f;
+	}
+
+	// We can have negative amplitudes as amplitudes are squared
+	#[ensures(ret > 0)]
+	fn flux_density(amplitude : f64) -> f64 {
+		return amplitude.pow(2) / (2 * Z0);
+	}
+
+	// TODO: stokes vector
+
+	#[requires(velocity >= 0, "Velocity must be greater than or equal to zero! (m/s)")]
+	#[requires(velocity < C, "You cannot go the speed of light!")]
+	#[requires(angle > 0 && angle < 2 * PI, "Angle (in radians) must be between 0 and 2PI")]
+	#[ensures(ret > 0)]
+	fn doppler_ratio(velocity : f64, angle : f64) -> f64 {
+		return sqrt(1 - velocity.pow(2) / C.pow(2)) / (1 - velocity * angle.cos() / C);
+	}
+
+	/**
+	* Computes the irradiance using a lambda which has either
+	* the incoming or outgoing L values. If Lincoming, returns the irradiance,
+	* if Loutgoing then returns the radiant exitance
+	* */
+	#[requires(theta > 0 && theta < 2 * PI, "Angle must be valid radial angle (rad)")]
+	#[requires(step.unwrap_or(0.01) > 0, "Cannot have zero or negative step for numerical integration.")]
+	#[ensures(ret > 0)]
+	fn irradiance(L : &dyn Fn(f64, f64) -> f64, step : Option<f64>) -> f64 {
+		let s : f64 = step.unwrap_or(0.01);
+		// Size of square for integration
+		let s2 : f64 = s.pow(2);
+		let mut sum : f64 = 0.0;
+		let mut theta : f64 = 0.0;
+		let mut phi : f64 = 0.0;
+		while theta < PI / 2 {
+			while phi < 2 * PI {
+				sum += s2 * L(theta, phi) * theta.cos() * theta.sin();
+				phi += s;
+			}
+			theta += s;
+		}
+		return sum;
+	}
+
+	/**
+	* Computes L_f (the spectral radiance) using the Rayleigh-Jeans approximation
+	* */
+	#[requires(temp > 0, "Cannot have zero or negative temperature (K)")]
+	#[requires(wavelength > 0, "Cannot have zero or negative wavelength (m)")]
+	fn spectral_radiance_f(temp : f64, f : f64) -> f64 {
+		return 2 * K * temp / wavelength.pow(2);
+	}
+
+	/**
+	* Computes L_lambda using Rayleigh-Jeans approximation
+	* */
+	#[requires(temp > 0, "Cannot have zero or negative temperature (K)")]
+	#[requires(wavelength > 0, "Cannot have zero or negative wavelength (m)")]
+	#[ensures(ret > 0)]
+	fn spectral_radiance_lambda(temp : f64, wavelength : f64) -> f64 {
+		return 2 * K * temp * C / wavelength.pow(4);
+	}
+
+	/**
+	* Computes total black body radiance
+	* */
+	#[requires(temp > 0, "Cannot have negative or absolute zero temperature!")]
+	#[ensures(ret > 0)]
+	fn bb_radiation(temp : f64) -> f64 {
+		return SIGMA * temp.pow(4);
+	}
+
+	/**
+	* Finds a temperature from emissivity
+	* */
+	// TODO
+
+	// TODO: Fraunhofer diffraction
+
+	// ===================== EM radiation interacting with matter =====================
+
+
+	// ===================== EM radiation interacting with Earths atmosphere =====================
+
+
 }
-
-fn em_wavelength(f : f64) -> f64 {
-    return C / f;
-}
-
-fn em_frequency(lambda : f64) -> f64 {
-    return C / lambda;
-}
-
-fn wave_num(wavelength : f64) -> {
-    return 2 * PI / wavelength;
-}
-
-fn photon_energy(f : f64) -> f64 {
-    return H * f;
-}
-
-fn flux_density(amplitude : f64) -> f64 {
-    return amplitude.pow(2) / (2 * Z0); 
-}
-
-// TODO: stokes vector
-
-fn doppler_ratio(velocity : f64, angle : f64) -> f64 {
-    return sqrt(1 - velocity.pow(2) / C.pow(2)) / (1 - velocity * cos(angle) / C);
-}
-
-/**
- * Computes the irradiance using a lambda which has either
- * the incoming or outgoing L values. If Lincoming, returns the irradiance,
- * if Loutgoing then returns the radiant exitance
- * */
-fn irradiance(L : &dyn Fn(f64, f64) -> f64, step : Option<f64>) -> f64 {
-    let s : f64 = step.unwrap_or(0.01);
-    // Size of square for integration
-    let s2 : f64 = s.pow(2);
-    let mut sum : f64 = 0.0;
-    let mut theta : f64 = 0.0;
-    let mut phi : f64 = 0.0;
-    while theta < PI / 2 {
-        while phi < 2 * PI {
-            sum += s2 * L(theta, phi) * cos(theta) * sin(theta);
-            phi += s;
-        }
-        theta += s;
-    }
-}
-
-/**
- * Computes L_f (the spectral radiance) using the Rayleigh-Jeans approximation
- * */
-fn spectral_radiance_f(temp : f64, wavelength : f64) -> f64 {
-    return 2 * K * temp / wavelength.pow(2);
-}
-
-/**
- * Computes L_lambda using Rayleigh-Jeans approximation
- * */
-fn spectral_radiance_lambda(temp : f64, wavelength : f64) -> f64 {
-    return 2 * K * temp * C / wavelength.pow(4);
-}
-
-/**
- * Computes total black body radiance
- * */
-fn bb_radiation(temp : f64) -> f64 {
-    return SIGMA * temp.pow(4);
-}
-
-/**
- * Finds a temperature from emissivity
- * */
-// TODO
-
-// TODO: Fraunhofer diffraction
-
-// ===================== EM radiation interacting with matter =====================
-
-
-// ===================== EM radiation interacting with Earths atmosphere =====================
